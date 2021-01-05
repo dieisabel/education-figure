b03ee786797ea6615d8a26222870fb950d95b2e3
Lavinchi <konstkadyr@gmail.com>
2021-01-05 19:02:46 +0200

    Убрал билды, добавил скрипты для компилирования

---
+++ build-windows.bat
@@ -0,0 +1,3 @@
+g++ .\src\main.cpp .\src\figure.cpp .\src\utils\math.cpp .\src\utils\point.cpp -lsfml-graphics -lsfml-window -lsfml-system -o .\dist\windows-build\figure.exe
+.\dist\windows-build\figure.exe
+del .\dist\windows-build\figure.exe
